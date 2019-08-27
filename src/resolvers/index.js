28c6c6a57655be4b1083bc2ac1fe4b9ce95b6f0b
Lawrence Velasquez <law.vzmk@gmail.com>
2019-08-27 12:24:22 -0600

    Add custom resolver for password field

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,5 +1,6 @@
 import Query from './Query/';
 import Mutation from './Mutation/';
+import User from './User';
 import Movie from './Movie';
 
-export default { Query, Mutation, Movie };
+export default { Query, Mutation, User, Movie };
